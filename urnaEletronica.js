ecb0f95a2449356e0b1379728661c4fd79a75961
Erick Hanon <142321063+ErickHanom@users.noreply.github.com>
2023-10-10 14:55:05 +0000

    feat: Começando estruturação da Urna

---
+++ urnaEletronica.js
@@ -0,0 +1,22 @@
+function urnaEletronica() {
+
+    let opcao;
+    let contador = 0;
+    
+
+    do {
+        
+        opcao = parseInt(prompt('Digite a opção:'));
+
+        console.log('Repetição', contador);
+
+        contador++;
+
+    } while (opcao !== 0);
+    
+    contador--;
+
+    console.log('Contagem', contador );
+
+}
+
