abe34a967212e263cb2ee2ecbe97414f16b86d04
Erick Hanon <erickheinom@gmail.com>
2023-11-01 11:59:34 -0300

    feat: Desafio extra #1 e #2

--- urnaEletronica.js
+++ urnaEletronica.js
@@ -1,21 +1,24 @@
-function verificarIntegridadeUrna() {
+async function verificarIntegridadeUrna() {
 
-    fetch('./urnaEletronica.js')
-        .them(conteudo =>  conteudo.text() )
-        .then(conteudo =>  CryptoJS.SHA256(conteudo).toString() )
-        .then(hashUrnaAtual => {
-            fetch('./hashVerificado.has')
-                .then(conteudo =>  conteudo.text() )
-                    .then(hashVerificado => {
-                    if (hashUrnaAtual === hashVerificado) {
-                        console.log('Hash verificado, urna íntegra.')
-                    } else {
-                        console.log('HASHES DIFERENTES, URNA ADULTERADA!!!')
-                        console.log(`Hash esperado: ${hashVerificado}`)
-                        console.log(`Hash da urna: ${hashUrnaAtual}`)
-                    }
-                })
-        });
+    // Biblioteca CryptoJS: https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.1.1/crypto-js.min.js
+
+    let hashUrnaAtual;
+    let hashVerificado;
+
+    await fetch('urnaEletronica.js')
+        .then(conteudo => conteudo.text())
+        .then(conteudo => CryptoJS.SHA256(conteudo).toString())
+        .then(conteudo => hashUrnaAtual = conteudo);
+    
+    await fetch('hashVerificado')
+        .then(conteudo => conteudo.text())
+        .then(conteudo => hashVerificado = conteudo);
+        
+    return {
+        status: hashUrnaAtual === hashVerificado,
+        hashUrnaAtual: hashUrnaAtual,
+        hashVerificado: hashVerificado
+    };
 
 }
 
@@ -24,8 +27,12 @@ function dataHoraAtual() {
     return data;
 }
 
+async function audioConfirmacao() {
+    const audio = new Audio ('./confirmacao.mp3')
+    await audio.play();
+}
 
-function urnaEletronica() {
+async function urnaEletronica() {
 
     // Declaração das variáveis
 
@@ -41,6 +48,8 @@ function urnaEletronica() {
     let votcandidato2 = 0;
     let votcandidato3 = 0;
     let votcandidato4 = 0;
+    
+    
 
 
     // console.log('Data:' + dataFim().toLocaleString());
@@ -98,7 +107,7 @@ function urnaEletronica() {
 
                 console.log('Voto computado para: ', candidato0[1])
                 votcandidato0++;
-
+                await audioConfirmacao();
             }
 
 
@@ -110,6 +119,7 @@ function urnaEletronica() {
 
                 console.log('Voto computado para: ', candidato1[1])
                 votcandidato1++;
+                await audioConfirmacao();
 
             }
         } else if (opcao == 13) {
@@ -120,6 +130,7 @@ function urnaEletronica() {
 
                 console.log('Voto computado para: ', candidato2[1])
                 votcandidato2++;
+                await audioConfirmacao();
 
             }
         } else if (opcao == 14) {
@@ -130,6 +141,7 @@ function urnaEletronica() {
 
                 console.log('Voto computado para: ', candidato3[1])
                 votcandidato3++;
+                await audioConfirmacao();
 
             }
         } else if (opcao == 15) {
@@ -140,10 +152,11 @@ function urnaEletronica() {
 
                 console.log('Voto computado para: ', candidato4[1])
                 votcandidato4++;
+                await audioConfirmacao();
 
             }
 
-        } else if (opcao == 5) {
+        } else if (opcao == 22) {
 
             if (confirm(
                 '\n Seu voto será computado para o Voto em Branco' +
@@ -151,6 +164,7 @@ function urnaEletronica() {
 
                 console.log('Voto computado para Voto em Branco ')
                 votobranco++;
+                await audioConfirmacao();
 
             }
         } else if (opcao === senhafinal) {
@@ -186,17 +200,17 @@ function urnaEletronica() {
         contador--;
 
         console.log('Contagem', contador);
-        console.log('Voto computado para Candidato1: ', votcandidato0);
-        console.log('Voto computado para Candidato2: ', votcandidato1);
-        console.log('Voto computado para Candidato3: ', votcandidato2);
-        console.log('Voto computado para Candidato4: ', votcandidato3);
-        console.log('Voto computado para Candidato5: ', votcandidato4);
+        console.log(`Voto computado para Candidato1: ${candidato0 [1]} - ` , votcandidato0);
+        console.log(`Voto computado para Candidato2: ${candidato1 [1]} - ` , votcandidato1);
+        console.log(`Voto computado para Candidato3: ${candidato2 [1]} - ` , votcandidato2);
+        console.log(`Voto computado para Candidato4: ${candidato3 [1]} - ` , votcandidato3);
+        console.log(`Voto computado para Candidato5: ${candidato4 [1]} - ` , votcandidato4);
         console.log('Voto em Branco computado', votobranco);
         console.log('Voto Nulo computado', votonulo);
 
 
         // Saída para usuário: boletim de urna
-        console.log('---------------------------------------------------')
+        console.log('*********************************************')
         console.log('** BOLETIM DE URNA **')
         console.log('O Total de votos computados é de: ', Totaldevotos);
 
@@ -242,12 +256,21 @@ function urnaEletronica() {
         console.log('Total de Votos: ' + Totaldevotos);
     }
 
-    verificarIntegridadeUrna();
-
+    console.log('******************************************')
+    
+    await verificarIntegridadeUrna().then(verificacao => {
+        if (verificacao.status) {
+            console.log('Hashes verificados, urna íntegra.');
+        } else {
+            console.log('URNA ADULTERADA!');
+            console.log(`Hash da urna: ${verificacao.hashUrnaAtual}`);
+            console.log(`Hash esperado: ${verificacao.hashVerificado}`);
+        }
+    });
+    
     console.log('Fim do Programa');
-    console.log('Horário de Término da Votação:' + '\n' + dataFim().toLocaleString()
-    )
-
+    console.log('Horário de Término da Votação:' + '\n' + dataFim().toLocaleString());
+    
     console.log('--------------------------------------------')
 
 }
