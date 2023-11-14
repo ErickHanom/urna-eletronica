f0d473f47847feebc56fff3ff1617d4c7fd9f169
Erick Hanon <erickheinom@gmail.com>
2023-11-14 13:26:40 +0000

    feat: Começo da Leitura Automática de candidatos

--- script.js
+++ script.js
@@ -2,11 +2,19 @@ document.getElementById('tela').innerHTML = localStorage.getItem('dados');
 
 
 function leituraDados(){
-    const dados ={
-       candidato: document.getElementById('candidato').value,
-       codigo: document.getElementById('codigo').value,
-       partido: document.getElementById('partido').value
-    } 
+    // const dados ={
+    //    candidato: document.getElementById('candidato').value,
+    //    codigo: document.getElementById('codigo').value,
+    //    partido: document.getElementById('partido').value
+    // } 
+
+    let config = urnaConfiguracao();
+
+    for(config = 0; config <= config.length; config++) {
+
+        document.write((`Nº: ${candidatos[i][0]} | Nome: ${candidatos [i] [1]} | Partido: ${candidatos [i] [2]}`));
+
+    }
 
     localStorage.setItem('dados', JSON.stringify(dados));
 
